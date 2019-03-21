ff7aab2a91f1098e42a3852281601adec22207f9
Beta <xmubeta@sina.com>
2019-03-21 21:08:17 +0800

    Update registry.rb

--- registry.rb
+++ registry.rb
@@ -5,7 +5,7 @@ control 'windows-base-101' do
     @link: https://msdn.microsoft.com/en-us/library/ms682586(v=vs.85).aspx
   '
   describe registry_key('HKLM\\System\\CurrentControlSet\\Control\\Session Manager') do
-    it { should exist }
+  
     it { should_not have_property_value('SafeDllSearchMode', :type_dword, '0') }
   end
 end
